8bdb6c72673ec5172a765c671ccec7a0a4a10e18
dazai-41 <minmax447@gmail.com>
2023-10-15 15:10:48 +1200

    update
    -

---
+++ assets/js/scroll.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $(document).on("scroll", function(){
+        var scrollPos = $(document).scrollTop();
+        $('.progress-bar a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+            if (refElement.length) {
+                var refElementTop = refElement.position().top;
+                var refElementBottom = refElementTop + refElement.outerHeight();
+                if (refElementTop <= scrollPos && refElementBottom > scrollPos) {
+                    $('.progress-bar a').removeClass("isActive");
+                    currLink.addClass("isActive");
+                } else {
+                    currLink.removeClass("isActive");
+                }
+            }
+        });
+    });
+});
