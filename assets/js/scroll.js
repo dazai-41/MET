3ca59686a92bd9e4ed3e759d6a90a0696d25c969
dazai-41 <minmax447@gmail.com>
2023-10-22 11:51:15 +1200

    update
    -

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,17 +1,19 @@
 $(document).ready(function () {
-    $(document).on("scroll", function(){
-    var scrollPos = $(document).scrollTop();
-    $('.progress-bar a').each(function () {
-    var currLink = $(this);
-    var refElement = $(currLink.attr("href"));
-    if (refElement.length) {
-    var refElementTop = refElement.position().top;
-    var refElementBottom = refElementTop + refElement.height();
-    if (refElementTop <= scrollPos && refElementBottom > scrollPos) {
-    $('.progress-bar a').removeClass("isActive");
-    currLink.addClass("isActive");
+    if($(window).width() >= 1280){   
+        $(document).on("scroll", function(){
+            var scrollPos = $(document).scrollTop();
+            $('.progress-bar a').each(function () {
+                var currLink = $(this);
+                var refElement = $(currLink.attr("href"));
+                if (refElement.length) {
+                    var refElementTop = refElement.position().top - 10;
+                    var refElementBottom = refElementTop + refElement.height();
+                    if (refElementTop <= scrollPos && refElementBottom > scrollPos) {
+                        $('.progress-bar a').removeClass("isActive");
+                        currLink.addClass("isActive");
+                    }
+                }
+            });
+        });
     }
-    }
-    });
-    });
-    });
+ });
